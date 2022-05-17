74991d5ea70ccb67785548b06c59a84f91f382e1
Nitin Singh <nitinsinghofficial9@gmail.com>
2022-05-17 14:24:09 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
+import { FeaturedPosts } from '../sections'
+
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto mb-8 px-10">
+      <Head>
+        <title>CMS Blog</title>
+        {/* <ink rel="icon" href="/favicon.ico"> */}
+      </Head>
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post, index) => (
+            <PostCard post={post.node} key={post.title} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  return {
+    props: { posts },
+  }
+}
